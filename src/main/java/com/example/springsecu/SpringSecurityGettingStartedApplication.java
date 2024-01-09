11b4bc986df8ef38ed172546e8e8db5c852bf9c6
alix babin <alix.babin69@gmail.com>
2024-01-09 10:19:53 +0100

    init

---
+++ src/main/java/com/example/springsecu/SpringSecurityGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGettingStartedApplication.class, args);
+	}
+
+}
